231ca9c6e9fc95f0ea689c7bbe6360c056adc2a5
JuneKim0712 <Junwoo.kim0712@gmail.com>
2021-06-10 15:35:38 -0700

    allowed different client to recieve message from other client and made the input and recieving format clean

--- client.py
+++ client.py
@@ -1,32 +1,43 @@
 import socket
-import time
+import threading
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+# change it if server is changed
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-USERNAME='JuneKim'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+
 def send(msg):
-    msg=str(msg)
+    msg = str(msg)
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print('sended')
 
-send(USERNAME)
 
-while True:
-    sending=input('send: ')
+def listen():
+    while True:
+        inMsg = client.recv(2 ** 16).decode(FORMAT)
+        print(inMsg + '\n' + '>>> ', end='')
+
+
+thread = threading.Thread(target=listen)
+thread.start()
+
+username = input('username: ')
+send(username)
+connection = True
+while connection:
+    sending = input('>>> ')
     if sending == 'disconnect':
+        send(DISCONNECT_MESSAGE)
         break
     send(sending)
-send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -11,36 +11,54 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+global allConn
+allConn = []
+
 
 def handel_client(conn, addr):
+    global allConn
     connected = True
     msg_length = conn.recv(HEADER).decode(FORMAT)
+
     if msg_length:
         msg_length = int(msg_length)
         msg = conn.recv(msg_length).decode(FORMAT)
         USERNAME = msg
         print('\n')
         print(f"{USERNAME} has connected")
-        conn.send("Msg recieved".encode(FORMAT))
+        allConn.append(conn)
+        send(f"{USERNAME} has connected", conn)
+
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
-            conn.send("Msg recieved".encode(FORMAT))
+
             if msg == DISCONNECT_MESSAGE:
                 print(f"{USERNAME} has disconnected")
+                send(f"{USERNAME} has disconnected", conn)
                 connected = False
                 break
             print(f"[{USERNAME}: {msg}]")
-            conn.send("Msg recieved".encode(FORMAT))
-            
+            send(f"{USERNAME}: {msg}", conn)
+
     conn.close()
+
+
+def send(message, conn):
+    global allConn
+    conns = allConn.copy()
+    conns.remove(conn)
+    message = message.encode(FORMAT)
+    for conn in conns:
+        conn.send(message)
 
 
 def start():
     server.listen()
     print(f"[Listening] Server is listening on {SERVER}")
+
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handel_client, args=(conn, addr))
